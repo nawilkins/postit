2015ff954484ab0112f109d1749a23e2268ac894
Newlin Wilkins <nwilkins89@gmail.com>
2014-05-04 21:48:36 -0500

    End of week 1 commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503224821) do
+ActiveRecord::Schema.define(version: 20140505005858) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140503224821) do
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "post_id"
   end
 
   create_table "post_categories", force: true do |t|
